9e0bb662bb87d620ba9dbd0f4e970cb10d66e825
errorsmonster <151611578+errorsmonster@users.noreply.github.com>
2024-02-16 21:31:26 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -264,12 +264,7 @@ Tɪᴘ: Sᴇʟᴇᴄᴛ "Lᴀɴɢᴜᴀɢᴇs" ᴏʀ "Sᴇᴀsᴏɴs" Bᴜᴛᴛ
 <b>🏷 𝚃𝚒𝚃𝚕𝙴: <a href={url}>{title}</a>
 🎭 𝙶𝚎𝚗𝚛𝚎𝚜: {genres}
 📆 𝚈𝚎𝚊𝚛: <a href={url}/releaseinfo>{year}</a>
-🌟 𝚁𝚊𝚝𝚒𝚗𝚐: <a href={url}/ratings>{rating}</a> / 10
-☀️ Languages : <code>{languages}</code>
-📀 RunTime: {runtime} Minutes
-📆 Release Info : {release_date}
-🎛 Countries : <code>{countries}</code>
-⏰ Result Shown in: {remaining_seconds}</b>"""
+🌟 𝚁𝚊𝚝𝚒𝚗𝚐: <a href={url}/ratings>{rating}</a> / 10</b>"""
     
     ALL_FILTERS = """
 <b>Hᴇʏ {}, Tʜᴇsᴇ ᴀʀᴇ ᴍʏ ᴛʜʀᴇᴇ ᴛʏᴘᴇs ᴏғ ғɪʟᴛᴇʀs.</b>"""
